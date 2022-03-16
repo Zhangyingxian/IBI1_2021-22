0c448607bb407e0dfa425eb79fbb46c01891d23c
Zhangyingxian <101192126+Zhangyingxian@users.noreply.github.com>
2022-03-16 15:04:00 +0800

    edit

---
+++ Practical5/triangle.py
@@ -0,0 +1,7 @@
+#calculate the number of the first ten triangles
+i=1
+x=0
+for i in range(1,11):
+ x=x+i
+ print (x)
+ 
---
+++ Practical5/variables.py
@@ -0,0 +1,13 @@
+a=19245301
+b=4218520
+c=271
+d=b-c
+e=a-b
+if d>e:
+ print ("2021 has the greatest number of new COVID-19 cases.")
+else:
+ print ("2022 has the greatest number of new COVID-19 cases.")
+X = "HELLO"
+Y = "HI"
+W = X and Y
+print (W)
