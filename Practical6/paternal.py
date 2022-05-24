a16bccf7efbc5091025024f5fdc7f5a819d53e9e
Zhangyingxian <101192126+Zhangyingxian@users.noreply.github.com>
2022-05-24 21:48:59 +0800

    Update paternal.py

--- Practical6/paternal.py
+++ Practical6/paternal.py
@@ -6,11 +6,17 @@ paternal_age=[30,35,40,45,50,55,60,65,70,75]
 chd=[1.03,1.07,1.11,1.17,1.23,1.32,1.42,1.55,1.72,1.94]
 #make scatter chart
 plt.scatter(paternal_age,chd,marker='d')
+#define the title
+plt.title('parental age vs offspring health') 
+#define the xlable
+plt.xlabel('parental age') 
+#define the ylable
+plt.ylabel('offspring health') 
 #let the scatter chart show
 plt.show()
 #make a dictionary
 relative = {30:1.03,35:1.07,40:1.11,45:1.17,50:1.23,55:1.32,60:1.42,65:1.55,70:1.72,75:1.94}
 print (relative)
-#a can be changedy
-a=35
-print ("When father's age is " + str(a) + ", " + "the risk of CHD is" + str(relative[a]))
+#input paternal age
+age= input ("paternal age: ")
+print ("When father's age is " + str(age) + ", " + "the risk of CHD is" + str(relative[a]))
