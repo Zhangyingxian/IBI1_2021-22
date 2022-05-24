44f8fe331eb7d9e35216d7fa24a38b5c20c6aac2
Zhangyingxian <101192126+Zhangyingxian@users.noreply.github.com>
2022-05-24 21:32:11 +0800

    Update triangle.py
    add pseudocode

--- Practical5/triangle.py
+++ Practical5/triangle.py
@@ -1,6 +1,8 @@
 #calculate the number of the first ten triangles
+#Every triangle has a dot on the top row
 i=1
 x=0
+#Every time go down a row, the number of points in that row goes up by 1 
 for i in range(1,11):
  x=x+i
  print (x)
