8fad53e54697f937c75eabb93e7b31433b6aef9c
Zhangyingxian <101192126+Zhangyingxian@users.noreply.github.com>
2022-05-24 21:36:17 +0800

    Update pizza_party.py
    add pseudocode

--- Practical5/pizza_party.py
+++ Practical5/pizza_party.py
@@ -2,6 +2,9 @@
 #print the number of knives cut when the number of the cake pieces is bigger than 64
 n=1
 p=0
+#Start with one cut
+#The formula can be used to calculate the number of cakes.
+#If the number of cakes is less than 64, increase the number of knives until the number of cakes is greater than 64
 while p<64:
  p=(n*n+n)/2
  n=n+1
